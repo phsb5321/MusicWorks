2c94ce9ba43b93e2e35381948bed3c7268bf613a
hunt0k4r <phsb52321@gmail.com>
2020-10-04 16:33:20 -0300

    temos tres telas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <ncurses.h>
 #include <iostream>
-
+#include<string>
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -10,23 +10,46 @@ int main(int argc, char const *argv[])
     noecho();
     cbreak();
 
-    int yMAx, xMax;
-    getmaxyx(stdscr, yMAx, xMax);
-
-    WINDOW *musicList = newwin(yMAx - 10, xMax / 2, 0, 0);
-    box(musicList, 0, 0);
+    int yMax, xMax;
+    getmaxyx(stdscr, yMax, xMax);
+     std::string s = std::to_string(yMax);
+    WINDOW *musicList = newwin(yMax , (xMax / 2)-1, 0, 0);
+    WINDOW *musicRow =newwin(yMax-3,(xMax/2)-1,3,(xMax/2)+1);
+    WINDOW *music =newwin(3,xMax/4,0,(xMax/2)+1);
     refresh();
+    box(musicRow,0,0);
+    wrefresh(musicRow);
+    box(musicList, 0, 0);
     wrefresh(musicList);
-
+    box(music,0,0);
+    wrefresh(music);
     keypad(musicList, true);
-
-    string musics[3] = {"musica_1", "musica_2", "musica_3"};
+    wattroff(music, A_REVERSE);
+    wrefresh(music);
+    int secondsLeft = 10;
+  do {
+      std::string n = std::to_string(secondsLeft);
+      string x="music_x:"+n;
+    mvwprintw(music,1,1,x.c_str());
+    wrefresh(music);
+    secondsLeft--;
+    werase(music);
+    box(music,0,0);
+    napms(1000);
+    
+    
+  } while (secondsLeft > 0);
+string musics[yMax-2] = {"Musics :"+s};
+    for (int i=1;i<sizeof(musics)/sizeof(musics[0]);i++){
+   std::string n = std::to_string(i);
+   musics[i]="music_"+n;
+    } 
     int choice;
     int highlight = 0;
-
-    while (true)
+   int count =20;
+    while (count>0)
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < sizeof(musics)/sizeof(musics[0]); i++)
         {
             if (i == highlight)
             {
@@ -46,15 +69,17 @@ int main(int argc, char const *argv[])
             break;
         case KEY_DOWN:
             highlight++;
-            if (highlight == 3)
-                highlight = 2;
-
+            if (highlight == sizeof(musics)/sizeof(musics[0]))
+                highlight = (sizeof(musics)/sizeof(musics[0]))-1;
+                break;
+        case KEY_RIGHT :
+         
         default:
             break;
-        }
+        }count--;
     }
 
     endwin();
-
+ 
     return 0;
 }
